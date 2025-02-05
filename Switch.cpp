93b35e196c917974d5dee45c52dc260089be72e6
Harsh Vaibhav <145591840+HARSH-20031972@users.noreply.github.com>
2025-02-05 12:58:36 +0530

    Add files via upload

---
+++ Switch.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int day=4;
+    switch(day) {
+        case 1:
+        cout << "Monday\n";
+        break;
+        case 2:
+        cout << "Tuesday\n";
+        break;
+        case 3:
+        cout << "Wednesday\n";
+        break;
+        case 4:
+        cout << "Thursday\n";
+        case 5:
+        cout << "Friday\n";
+        case 6:
+        cout << "Saturday\n";
+        case 7:
+        cout << "Sunday\n";
+    }
+    return 0;
+}
