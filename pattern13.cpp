47e7ba2572aa69f3d1f5c7c98cc87a8466236eb3
Harsh Vaibhav <145591840+HARSH-20031972@users.noreply.github.com>
2025-02-10 18:32:01 +0530

    Add files via upload

---
+++ pattern13.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void print13(int n) {
+	int num=1;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=i;j++) {
+			cout << num << " ";
+			num=num+1;
+		}
+		cout << endl;
+	}
+}
+int main() {
+	int x;
+	cout << "Enter Layer=";
+	cin >> x;
+	print13(x);
+}
